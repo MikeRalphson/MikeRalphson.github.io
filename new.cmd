c58d168b75a068565286d07d372980b6694bd4c2
Mike Ralphson <mike.ralphson@gmail.com>
2016-05-13 23:25:03 +0100

    Publish blog

--- new.cmd
+++ new.cmd
@@ -2,4 +2,4 @@
 SET name=%1
 IF "%name%" == "" SET name=Untitled
 FOR /F "delims=" %%i IN ('unixdate') DO set today=%%i
-COPY _drafts\blank.md _posts\%today%_%name%.md
+COPY _drafts\blank.md _posts\%today%-%name%.md
